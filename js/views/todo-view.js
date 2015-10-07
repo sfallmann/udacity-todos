24619d3d9724f3a7d5a317e3a53c6a3cb616a1f8
sfallmann <sfallmann@pbmbrands.com>
2015-10-07 14:01:08 -0400

    fixed priority button - saves status

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -23,7 +23,8 @@ var app = app || {};
             'click .destroy': 'clear',
             'keypress .edit': 'updateOnEnter',
             'keydown .edit': 'revertOnEscape',
-            'blur .edit': 'close'
+            'blur .edit': 'close',
+            'click .priority-btn' : 'prioritize'
         },
 
         // The TodoView listens for changes to its model, re-rendering. Since
@@ -51,6 +52,7 @@ var app = app || {};
 
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggleClass('completed', this.model.get('completed'));
+            this.$el.toggleClass('priority', this.model.get('priority'));
             this.toggleVisible();
             this.$input = this.$('.edit');
             return this;
@@ -58,6 +60,10 @@ var app = app || {};
 
         toggleVisible: function () {
             this.$el.toggleClass('hidden', this.isHidden());
+        },
+
+        prioritize: function(){
+            this.model.prioritize();
         },
 
         isHidden: function () {
